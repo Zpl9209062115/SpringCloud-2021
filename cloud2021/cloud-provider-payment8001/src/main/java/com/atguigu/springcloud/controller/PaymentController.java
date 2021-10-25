4be62934c89314527b50402d9edd5a8b598b258f
ZPengLin <zhaopl9209062115@163.com>
2021-10-25 17:35:55 +0800

    8001 eureka 生产者初搭建

---
+++ cloud2021/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,42 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Autowired
+    PaymentService paymentService;
+
+
+    @GetMapping("/payment/{id}")
+    public CommonResult find(@PathVariable("id") long id){
+        Payment payment = paymentService.getPaymentById(id);
+        if(payment != null){
+            return new CommonResult(200,"查询成功" + serverPort,payment);
+        }else{
+            return new CommonResult(444,"查询失败" + serverPort,null);
+        }
+    }
+
+    @PostMapping("/payment/create")
+    public CommonResult add(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        if (result > 0){
+            return new CommonResult(200,"插入数据库成功" + serverPort,result);
+        }else{
+            return new CommonResult(444,"插入数据库失败" + serverPort,null);
+        }
+    }
+
+}
